61e8eabd85abbb933ae55a288b7382316d1db214
litianyi <574399358@qq.com>
2023-01-23 13:16:17 +0800

    项目初始化

---
+++ src/main/java/com/liwell/cinema/CinemaApplication.java
@@ -0,0 +1,19 @@
+package com.liwell.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author Li
+ * @date Created on 2023/1/23
+ */
+@SpringBootApplication
+public class CinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
+
+}
