c6dc5a49943b4f6c0c1900a7740679891e60109f
litianyi <574399358@qq.com>
2023-03-26 23:26:25 +0800

    使用 satoken 作为安全框架

--- src/main/java/com/liwell/cinema/domain/po/LoginUser.java
+++ src/main/java/com/liwell/cinema/domain/po/LoginUser.java
@@ -1,62 +1,62 @@
-package com.liwell.cinema.domain.po;
-
-import com.liwell.cinema.domain.entity.User;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-
-/**
- * Description:
- *
- * @author Li
- * @date Created on 2023/3/16
- */
-public class LoginUser implements UserDetails {
-
-    private User user;
-
-    public LoginUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    public Integer getId() {
-        return user.getId();
-    }
-
-}
+//package com.liwell.cinema.domain.po;
+//
+//import com.liwell.cinema.domain.entity.User;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//
+///**
+// * Description:
+// *
+// * @author Li
+// * @date Created on 2023/3/16
+// */
+//public class LoginUser implements UserDetails {
+//
+//    private User user;
+//
+//    public LoginUser(User user) {
+//        this.user = user;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//
+//    public Integer getId() {
+//        return user.getId();
+//    }
+//
+//}
--- src/main/java/com/liwell/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/liwell/cinema/service/impl/UserServiceImpl.java
@@ -1,26 +1,21 @@
 package com.liwell.cinema.service.impl;
 
-import cn.hutool.jwt.JWTUtil;
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.liwell.cinema.domain.constants.Constants;
 import com.liwell.cinema.domain.dto.LoginDTO;
 import com.liwell.cinema.domain.entity.User;
 import com.liwell.cinema.domain.enums.ResultEnum;
-import com.liwell.cinema.domain.po.LoginUser;
+import com.liwell.cinema.domain.enums.StateEnum;
 import com.liwell.cinema.domain.vo.LoginVO;
 import com.liwell.cinema.exception.ResultException;
 import com.liwell.cinema.helper.RedisHelper;
 import com.liwell.cinema.mapper.UserMapper;
 import com.liwell.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -33,38 +28,25 @@ import java.util.Objects;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
     private RedisHelper redisHelper;
 
     @Override
     public LoginVO login(LoginDTO dto) {
-        UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(token);
-        if (Objects.isNull(authenticate)) {
+        User user = baseMapper.selectOne(new QueryWrapper<User>()
+                .eq("username", dto.getUsername()).eq("state", StateEnum.VALID));
+        if (Objects.isNull(user) || !dto.getPassword().equals(user.getPassword())) {
             throw new ResultException(ResultEnum.USER_PWD_ERROR);
         }
-        LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
-        Integer userId = loginUser.getId();
-        redisHelper.setLoginUser(userId, loginUser);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("userId", userId);
-        map.put("expire_time", System.currentTimeMillis() + 60 * 60 * 1000);
-        String jwtToken = JWTUtil.createToken(map, Constants.JWT_KEY.getBytes());
+        StpUtil.login(user.getId());
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
         LoginVO loginVO = new LoginVO();
-        loginVO.setToken(jwtToken);
+        loginVO.setToken(tokenInfo.getTokenValue());
         return loginVO;
     }
 
     @Override
     public void logout() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
-        Integer userId = loginUser.getId();
-        redisHelper.removeLoginUser(userId);
-        SecurityContextHolder.getContext().setAuthentication(null);
+        StpUtil.logout();
     }
 
 }
